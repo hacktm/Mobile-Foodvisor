2d56eb9f6e0eed0110f073cb45c7988a97823e7d
adi <adrian.dumitru.68@yahoo.com>
2014-10-18 15:54:02 +0300

    Config.php

--- web/engine/config.php
+++ web/engine/config.php
@@ -1,8 +1,8 @@
 <?php
 
  	define("MYSQL_HOST",'localhost');
- 	define("MYSQL_USER",'guser');
- 	define("MYSQL_PASS",'1234');
- 	define("MYSQL_DB",'foodvisor');
+ 	define("MYSQL_USER",'websotrm_fv');
+ 	define("MYSQL_PASS",'memestoica123');
+ 	define("MYSQL_DB",'websotrm_fv');
 
 ?>
