ccdb021e32b269ae61a85b0d1cb96b9d519415e6
raulursu <raul@webstorm.ro>
2014-10-18 17:30:46 +0300

    adaugare constanta

--- web/engine/config.php
+++ web/engine/config.php
@@ -1,8 +1,9 @@
 <?php
 
- 	define("MYSQL_HOST",'localhost');
- 	define("MYSQL_USER",'webstorm_fv');
- 	define("MYSQL_PASS",'memestoica123');
- 	define("MYSQL_DB",'webstorm_fv');
+define("MYSQL_HOST",'localhost');
+define("MYSQL_USER",'webstorm_fv');
+define("MYSQL_PASS",'memestoica123');
+define("MYSQL_DB",'webstorm_fv');
+define("API_KEY","meme");
 
 ?>
